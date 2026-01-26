c9c282b752979a2ae948d79651277f46922aa3fc
Sasha Mattison <sashastoolbox@gmail.com>
2026-01-26 14:28:46 -0800

    .

--- MacOS/src/main.cpp
+++ MacOS/src/main.cpp
@@ -133,7 +133,7 @@ int main() {
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle.data(), GL_DYNAMIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 10000, triangle.data() ,GL_DYNAMIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -190,6 +190,7 @@ int main() {
 
     // ----- Render Loop -----
     while (!glfwWindowShouldClose(window)) {
+
         processInput(window);
 
         glClearColor(0.0f, 0.3f, 0.7f, 1.0f);
