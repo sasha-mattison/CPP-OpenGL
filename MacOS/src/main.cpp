4774d29620c21a43a24cfccb462aa894c1342dfc
Sasha Mattison <sashastoolbox@gmail.com>
2026-01-27 10:43:59 -0800

    .

--- MacOS/src/main.cpp
+++ MacOS/src/main.cpp
@@ -1,131 +1,103 @@
 // OpenGL + Graphics Libraries
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+// Math
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <stb/stb_image.h>
 
 // Other Libraries
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <unistd.h>
 #include <vector>
 
+// Engine
+#include "../shaders/shader.h"
 
-int windowWidth = 800, windowHeight = 600;
-bool apple = false;
+int windowWidth = 800;
+int windowHeight = 600;
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    glViewport(0, 0, width, height);
-    windowWidth = width;
-    windowHeight = height;
+bool fullscreen = false;
+int windowedWidth = 800, windowedHeight = 600;
+int windowedX = 100, windowedY = 100;
 
-    if(apple) {
-            std::cout << windowWidth/2 << " " << windowHeight/2 << std::endl;
-    }
-    else {
-    std::cout << windowWidth << " " << windowHeight << std::endl;
-    }
-}
+void toggleFullscreen(GLFWwindow* window) {
+    fullscreen = !fullscreen;
 
-void processInput(GLFWwindow *window) {
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true); 
-}
+    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode* mode = glfwGetVideoMode(monitor);
 
-std::vector<float> worldspaceConverter(int width, int height, float xVal, float yVal, bool normalized) {
-    if (!normalized) {
-        return {
-            xVal / width,
-            yVal / height,
-        };
+    if (fullscreen) {
+        glfwGetWindowPos(window, &windowedX, &windowedY);
+        glfwGetWindowSize(window, &windowedWidth, &windowedHeight);
+
+        glfwSetWindowMonitor(
+            window,
+            monitor,
+            0, 0,
+            mode->width,
+            mode->height,
+            mode->refreshRate
+        );
     } else {
-        return {
-            xVal * width,
-            yVal * height
-        };
+        glfwSetWindowMonitor(
+            window,
+            nullptr,
+            windowedX,
+            windowedY,
+            windowedWidth,
+            windowedHeight,
+            0
+        );
     }
 }
 
-std::vector<float> p1 = {-0.5f, -0.5f, 0.0f};
-std::vector<float> p2 = {0.5f, -0.5f, 0.0f};
-std::vector<float> p3 = {0.5f, -0.5f, 0.0f};
+void processInput(GLFWwindow* window) {
+    static bool fPressedLastFrame = false;
 
-std::vector<float> convertVectors(std::vector<float> v1, std::vector<float> v2, std::vector<float> v3) {
-    size_t vecSize = v1.size() + v2.size() + v3.size();
-    std::vector<float> finalVector;
-    finalVector.reserve(vecSize);
-    finalVector.insert(finalVector.end(), v1.begin(), v1.end());
-    finalVector.insert(finalVector.end(), v2.begin(), v2.end());
-    finalVector.insert(finalVector.end(), v3.begin(), v3.end());
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 
-    return finalVector;
-}
-
-
-
-float vertices[] = {
-        -0.5f, -0.5f, 0.0f, 
-        0.5f, -0.5f, 0.0f, 
-        0.5f, -0.5f, 0.0f, 
-
-};
-
-
-void changeVertices(int arrayLength, float * array) {
-    std::cout << array[1] << std::endl;
-}
-
-
-// Load shader source from file
-std::string loadShaderSrc(const char* filename) {
-    std::ifstream file(filename);
-    std::stringstream buf;
-    std::string ret = "";
-
-    if (file.is_open()) {
-        buf << file.rdbuf();
-        ret = buf.str();
-    } else {
-        std::cout << "Could not open shader file: " << filename << std::endl;
+    bool fPressed = glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS;
+    if (fPressed && !fPressedLastFrame) {
+        toggleFullscreen(window);
     }
-    file.close();
-    return ret;
+
+    fPressedLastFrame = fPressed;
 }
 
 int main() {
-    std::cout << "Program Started" << std::endl <<std::endl;
-    std::vector<float> triangle = convertVectors(p1, p2, p3);
+    std::cout << "Program Started" << std::endl << std::endl;
 
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    apple = true;
 #endif
 
-    GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "OpenGL Window", NULL, NULL);
-    if (!window) {
-        std::cout << "Could not create window\n";
-        glfwTerminate();
-        return -1;
-    }   
+    GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "OpenGL Window", nullptr, nullptr);
 
-    glfwMakeContextCurrent(window);
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cout << "Failed to initialize GLAD\n";
+    if (!window) {
+        std::cerr << "Failed to create GLFW window\n";
+        glfwTerminate();
         return -1;
     }
 
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwMakeContextCurrent(window);
 
-    // ----- VAO and VBO Setup -----
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD\n";
+        return -1;
+    }
+
+    std::vector<float> vertices = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
     unsigned int VAO, VBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -133,75 +105,37 @@ int main() {
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, 10000, triangle.data() ,GL_DYNAMIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_DYNAMIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0
+    );
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
-    // ----- Shader Setup -----
-    int success;
-    char infoLog[512];
+    Shader shader(
+        "../shaders/vertex_core.glsl",
+        "../shaders/fragment_core.glsl"
+    );
 
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    std::string vertShaderSrc = loadShaderSrc("../shaders/vertex_core.glsl");
-    const char* vertShader = vertShaderSrc.c_str();
-    glShaderSource(vertexShader, 1, &vertShader, NULL);
-    glCompileShader(vertexShader);
-
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "Vertex shader compilation error:\n" << infoLog << std::endl;
-    }
-
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    std::string fragShaderSrc = loadShaderSrc("../shaders/fragment_core.glsl");
-    const char* fragShader = fragShaderSrc.c_str();
-    glShaderSource(fragmentShader, 1, &fragShader, NULL);
-    glCompileShader(fragmentShader);
-
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "Fragment shader compilation error:\n" << infoLog << std::endl;
-    }
-
-    // Shader program
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "Shader linking error:\n" << infoLog << std::endl;
-    }
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    //Testing
-
-    
-
-    // ----- Render Loop -----
+    //Render Loop
     while (!glfwWindowShouldClose(window)) {
-
         processInput(window);
 
         glClearColor(0.0f, 0.3f, 0.7f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        glUseProgram(shaderProgram);
+
+        shader.use();
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 
     glfwTerminate();
     return 0;
